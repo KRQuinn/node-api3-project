4394389edcbaddcd5d32a31a848b669fc44cf8c5
KRQuinn <kr.quinn82@gmail.com>
2021-10-27 22:10:12 -0700

    testing is being inconsistent, testing if codegrade environment reports differently. Should be at MVP

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,13 +5,12 @@ const express = require('express');
 const {  
   handleError,
   validateUserId,
-  validateUser,
-  // validateUser, 
-  // validatePost
+  validateUser, 
+  validatePost
 } = require('../middleware/middleware');
 
 const Users = require('./users-model');
-// const Posts = require('../posts/posts-model.js');
+const Posts = require('../posts/posts-model.js');
 
 const router = express.Router();
 
@@ -46,26 +45,43 @@ router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // and another middleware to check that the request body is valid
   const { id } = req.params
   Users.update(id, req.body)
-    .then(user => {
+    .then((user) => {
       res.status(200).json(user)
     })
     .catch(next)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, async (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  const { id } = req.params
+  const deletedUser = await Users.getById(id)
+  Users.remove(id)
+    .then(() => {
+      res.status(200).json(deletedUser)
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  const { id } = req.params
+  Users.getUserPosts(id)
+    .then((post) => {
+      res.status(200).json(post)
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  req.body.user_id = req.params.id
+  Posts.insert(req.body)
+    .then((post) => {
+      res.status(201).json(post)
+    })
+    .catch(next)
 });
 
 router.use(handleError);
